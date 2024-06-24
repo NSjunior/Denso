ce22092d1ecbc62defafc35db03b3f4d2354c85a
NSjunior <junior.sw.nextschool@gmail.com>
2024-06-24 15:14:22 +0700

    merge

--- backend/controllers/VehicleRequestController.php
+++ backend/controllers/VehicleRequestController.php
@@ -111,7 +111,6 @@ class VehicleRequestController extends Controller
      * @return string|\yii\web\Response
      * @throws NotFoundHttpException if the model cannot be found
      */
-
     public function actionUpdate($id)
     {
         $model = $this->findModel($id);
